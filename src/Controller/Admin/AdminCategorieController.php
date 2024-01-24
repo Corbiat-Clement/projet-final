e15b5bb2af01b38e129981f45bc26557f26fd335
Corbiat Clement <ccorbiat@test.com>
2024-01-24 20:01:57 +0100

    Interface Admin

---
+++ src/Controller/Admin/AdminCategorieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/categorie', name: 'admin_categorie_')]
+class AdminCategorieController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategorieRepository $categorieRepository): Response
+    {   
+        $categorie = $categorieRepository->findBy([], ['parent' => 'asc', 'name' => 'asc']);
+
+
+        return $this->render('admin/categorie/index.html.twig', compact('categorie'));
+    }
+}
